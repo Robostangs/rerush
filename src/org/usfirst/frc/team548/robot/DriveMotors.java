4aa60aab764d3944f1f7ab32e501005068712720
wmod <spamgoeshere35@gmail.com>
2015-01-08 19:07:30 -0500

    Fixed semicolon in DriveMotors

---
+++ src/org/usfirst/frc/team548/robot/DriveMotors.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team548.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class DriveMotors {
+	
+	private static DriveMotors instance = null;
+	private static Talon rightFront, rightMid, rightBack, leftFront, leftMid, leftBack, center;
+	
+	private DriveMotors() {
+    	rightFront = new Talon(Constants.DT_RIGHT_FRONT_TALON_POS);
+    	rightMid = new Talon(Constants.DT_RIGHT_MID_TALON_POS);
+    	rightBack = new Talon(Constants.DT_RIGHT_BACK_TALON_POS);
+    	leftFront = new Talon(Constants.DT_LEFT_FRONT_TALON_POS);
+    	leftMid = new Talon(Constants.DT_LEFT_MID_TALON_POS);
+    	leftBack = new Talon(Constants.DT_LEFT_BACK_TALON_POS);
+    	center = new Talon(Constants.DT_CENTER_TALON_POS);
+	}
+
+	public static DriveMotors getInstance() {
+		if(instance == null) {
+			instance = new DriveMotors();
+		}
+		return instance;
+	}
+	
+	public static void drive(double left, double right) {
+		rightFront.set(-left);
+		rightMid.set(-left);
+		rightBack.set(-left);
+		leftFront.set(right);
+		leftMid.set(right);
+		leftBack.set(right);
+	}
+}
