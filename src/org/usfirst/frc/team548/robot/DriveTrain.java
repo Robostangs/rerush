5fc7e18c308d096715601fba63236c2c6692d781
wmod <spamgoeshere35@gmail.com>
2015-01-08 19:04:45 -0500

    Migrated Talons to DriveMotors.java

--- src/org/usfirst/frc/team548/robot/DriveTrain.java
+++ src/org/usfirst/frc/team548/robot/DriveTrain.java
@@ -5,16 +5,9 @@ import edu.wpi.first.wpilibj.Talon;
 public class DriveTrain {
 	
 	private static DriveTrain instance = null; 
-	private static Talon rightFront, rightMid, rightBack, leftFront, leftMid, leftBack, center;
 	
 	private DriveTrain() {
-    	rightFront = new Talon(Constants.DT_RIGHT_FRONT_TALON_POS);
-    	rightMid = new Talon(Constants.DT_RIGHT_MID_TALON_POS);
-    	rightBack = new Talon(Constants.DT_RIGHT_BACK_TALON_POS);
-    	leftFront = new Talon(Constants.DT_LEFT_FRONT_TALON_POS);
-    	leftMid = new Talon(Constants.DT_LEFT_MID_TALON_POS);
-    	leftBack = new Talon(Constants.DT_LEFT_BACK_TALON_POS);
-    	center = new Talon(Constants.DT_CENTER_TALON_POS);
+		DriveMotors.getInstance()
 	}
 	
 	public static DriveTrain getInstance() {
@@ -23,21 +16,12 @@ public class DriveTrain {
 		}
 		return instance;
 	}
-	
-	public static void drive(double left, double right) {
-		rightFront.set(-left);
-		rightMid.set(-left);
-		rightBack.set(-left);
-		leftFront.set(right);
-		leftMid.set(right);
-		leftBack.set(right);
-	}
-	
+
 	public static void humanDrive(double left, double right) {
 		if(Math.abs(left) < 0.2 && Math.abs(right) < 0.2) {
-			drive(0, 0);
+			DriveMotors.drive(0, 0);
 		} else {
-			drive(left, right);
+			DriveMotors.drive(left, right);
 		}
 	}
 }
