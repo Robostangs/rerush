cd780ddac64ba0cd2c91950494f215a2bb8ccd1c
orangelight <alex.deneau17@gmail.com>
2015-01-10 17:04:59 -0500

    Chnaged CAN type

--- src/org/usfirst/frc/team548/robot/DriveMotors.java
+++ src/org/usfirst/frc/team548/robot/DriveMotors.java
@@ -1,19 +1,19 @@
 package org.usfirst.frc.team548.robot;
 
-import edu.wpi.first.wpilibj.hal.CanTalonSRX;
+import edu.wpi.first.wpilibj.CANTalon;
 
 public class DriveMotors {
 	
 	private static DriveMotors instance = null;
-	private static CanTalonSRX rightFront, rightBack, leftFront, leftBack, centerBack, centerFront;
+	private static CANTalon rightFront, rightBack, leftFront, leftBack, centerBack, centerFront;
 	
 	private DriveMotors() {
-    	rightFront = new CanTalonSRX(Constants.DT_RIGHT_FRONT_TALON_POS);
-    	rightBack = new CanTalonSRX(Constants.DT_RIGHT_BACK_TALON_POS);
-    	leftFront = new CanTalonSRX(Constants.DT_LEFT_FRONT_TALON_POS);
-    	leftBack = new CanTalonSRX(Constants.DT_LEFT_BACK_TALON_POS);
-    	centerBack = new CanTalonSRX(Constants.DT_CENTER_BACK_TALON_POS);
-    	centerFront = new CanTalonSRX(Constants.DT_CENTER_FRONT_TALON_POS);
+    	rightFront = new CANTalon(Constants.DT_RIGHT_FRONT_TALON_POS);
+    	rightBack = new CANTalon(Constants.DT_RIGHT_BACK_TALON_POS);
+    	leftFront = new CANTalon(Constants.DT_LEFT_FRONT_TALON_POS);
+    	leftBack = new CANTalon(Constants.DT_LEFT_BACK_TALON_POS);
+    	centerBack = new CANTalon(Constants.DT_CENTER_BACK_TALON_POS);
+    	centerFront = new CANTalon(Constants.DT_CENTER_FRONT_TALON_POS);
 	}
 
 	public static DriveMotors getInstance() {
@@ -24,25 +24,25 @@ public class DriveMotors {
 	}
 	
 	public static void drive(double left, double right) {
-		rightFront.Set(-left);
-		rightBack.Set(-left);
-		leftFront.Set(right);
-		leftBack.Set(right);
+		rightFront.set(-left);
+		rightBack.set(-left);
+		leftFront.set(right);
+		leftBack.set(right);
 	}
 
 	
 	public static void driveCenter(double power) {
-		centerBack.Set(power);
-		centerFront.Set(-power);
+		centerBack.set(power);
+		centerFront.set(-power);
 	}
 	
 	public static void stopMotors() {
-		rightFront.Set(0);
-		rightBack.Set(0);
-		leftFront.Set(0);
-		leftBack.Set(0);
-		centerBack.Set(0);
-		centerFront.Set(0);
+		rightFront.set(0);
+		rightBack.set(0);
+		leftFront.set(0);
+		leftBack.set(0);
+		centerBack.set(0);
+		centerFront.set(0);
 	}
 
 }
