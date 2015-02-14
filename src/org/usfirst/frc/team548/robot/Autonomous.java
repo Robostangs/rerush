46bd3f7f4aa57280f4bcc7b68bb8bdb53078d60f
orangelight <alex.deneau17@gmail.com>
2015-02-13 21:26:15 -0500

    Added auto switch with mode numbers

--- src/org/usfirst/frc/team548/robot/Autonomous.java
+++ src/org/usfirst/frc/team548/robot/Autonomous.java
@@ -5,6 +5,7 @@ public class Autonomous {
 	private static Autonomous instance;
 	private static boolean hasReachedContainer = false;
 	private static boolean done = false;
+	private static int mode = 1;
 	
 	private Autonomous() {
 
@@ -19,13 +20,42 @@ public class Autonomous {
 	
 	public static void run() {
 		
-		//Drive into auto zone
+		
+		switch (Autonomous.getAutoMode()) {
+		// Drive into auto zone
+		case 1:
+			Autonomous.driveToAutoZone();
+			break;
+		// Drive into auto zone with container
+		case 2:
+			Autonomous.driveToAutoZoneWithContainer();
+			break;
+		// Drive into auto zone with container and tote
+		case 3:
+			Autonomous.driveToAutoZoneWithToteAndContainer();
+			break;
+		// Pick up container and tote do 180 pick up tote drive straight pick up last tote then turn into auto zone
+		case 4:
+			Autonomous.thinkOfAGoodNameForThidMethodForAuto4();
+			break;
+		//  Pick up container and tote do 180 pick up tote drive around container pick up last tote then turn into auto zone
+		case 5: 
+			Autonomous.godTierAuto();
+			break;
+		default: 
+			Autonomous.driveToAutoZone();
+			break;
+		}
+	}
+	
+	private static void driveToAutoZone() {
 		if(!done) {
 			DriveTrain.driveDistance(Constants.AUTON_DRIVE_DISTANCE_ROBOT_SET);
 			done = true;
 		}
-		
-		//Drive into auto zone with container
+	}
+	
+	private static void driveToAutoZoneWithContainer() {
 		if(!done) {
 			if(!hasReachedContainer) {
 				if(DriveMotors.getEncoderAverage() != Constants.AUTON_DISTANCE_TO_CONTAINER) {
@@ -39,8 +69,25 @@ public class Autonomous {
 			done = true;
 			}
 		}
-		
+	}
+	
+	private static void driveToAutoZoneWithToteAndContainer() {
 		
 	}
-
+	
+	private static void thinkOfAGoodNameForThidMethodForAuto4() {
+		
+	}
+	
+	private static void godTierAuto() {
+		
+	}
+	
+	public static void setAutoMode(int mode) {
+		Autonomous.mode = mode;
+	}
+	
+	public static int getAutoMode() {
+		return Autonomous.mode;
+	}
 }
